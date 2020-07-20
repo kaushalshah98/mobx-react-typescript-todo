2f138f1016ea8caebc377432fede5ba0eed325f6
Kaushal Shah <kaushalshah589@gmail.com>
2020-07-21 00:45:32 +0530

    new syntax added for class

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, FC, } from "react";
+import React, { Fragment, PureComponent } from "react";
 import NavBar from "./components/navbar/navbar";
 import Container from "./components/to-do-container/container";
 import { library } from "@fortawesome/fontawesome-svg-core";
@@ -9,14 +9,17 @@ import "./App.css";
 
 library.add(faTrash);
 
-const App: FC<any> = () => {
-  const { totalItems } = DataList;
-  return (
-    <Fragment>
-      <NavBar totalItems={totalItems}></NavBar>
-      <Container></Container>
-    </Fragment>
-  );
+@observer
+class App extends PureComponent {
+  render() {
+    const { totalItems } = DataList;
+    return (
+      <Fragment>
+        <NavBar totalItems={totalItems}></NavBar>
+        <Container></Container>
+      </Fragment>
+    );
+  }
 };
 
-export default observer(App);
+export default App;
